2f569e2c519971c952cfee01d9841941e92b1f4b
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 09:51:58 +0600

    function practice done

---
+++ function.js
@@ -0,0 +1,29 @@
+// function
+
+function startFan(){
+    console.log("stand up")
+    console.log("go to the switch board")
+
+    console.log("on the fan")
+
+}
+startFan();
+// ans :-
+// stand up
+// go to the switch board
+// on the fan
+
+
+
+
+function giveSingara(money){
+
+    console.log("mama singara dao")
+
+    console.log(money)
+}
+giveSingara(200)
+// ans :-
+// mama singara dao      
+// 200
+
