e3c1f6ace4bc44eff32d87597f0fd9e2c05cfa4b
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 09:57:39 +0600

    objrct pracrice done

---
+++ object.js
@@ -0,0 +1,25 @@
+// object 
+// mobile and student child known as properties
+
+var student1 = {
+    id:126,
+    name: "solaiman khan",
+
+    class: 11,
+    marks: 988
+ }
+console.log(student1)
+// ans :- { id: 126, name: 'solaiman khan', class: 11, marks: 988 }
+
+console.log(student1.marks) // ans :- 988
+
+
+var mobile = {
+  brand: 'oppo',
+  price: 28000,
+  storage: "64gb",
+  camera: "12mp"
+
+}            
+
+
