5872b3c78f2b1a164449b64ad71f61dc8fa55ac1
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 09:54:46 +0600

    return practice done

---
+++ return_method.js
@@ -0,0 +1,24 @@
+// return
+
+function add(number1, number2) {
+
+    var sum = number1 + number2;
+
+    return sum;
+}
+var plus = add(100, 50);
+console.log(plus)
+// ans :- 150
+
+
+function singaraLagbe(money) {
+    var singaraPrice = 10;
+    var quantity = money / singaraPrice;
+
+    return quantity;
+}
+var singaraDao = singaraLagbe(200);
+console.log("amake singara dao",singaraDao,"peach")
+
+// ans :- amake singara dao 20 peach
+
