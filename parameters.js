3121983397e2cf9ed979c4bf7bb9f26d70ff83a3
tanvir491981 <89649985+tanvir491@users.noreply.github.com>
2023-05-04 09:53:45 +0600

    parameters practice done

---
+++ parameters.js
@@ -0,0 +1,22 @@
+// parameter
+
+function add(num1,num2){
+    console.log("going to add", num1, num2)
+
+}
+add(10,20)
+// ans :- going to add 10 20
+
+
+function sum(a, b, c, d, e) {
+
+    console.log(a, b, c, d, e)
+
+    // ans :- 15 25 35 45 55
+
+    var sum = a+b+c+d+e;    
+
+    console.log(sum)
+    // ans :- 175
+}
+sum(15,25,35,45,55);
